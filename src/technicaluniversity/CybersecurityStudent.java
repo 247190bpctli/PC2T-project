1c1a39caaa3f09edd198f88fca9a8028f0d4b9b1
Vojtěch Trunda <vojtech.trunda@vojtat.cz>
2025-03-30 18:40:09 +0200

    Student specializations, constructors

--- src/technicaluniversity/CybersecurityStudent.java
+++ src/technicaluniversity/CybersecurityStudent.java
@@ -1,5 +1,12 @@
 package technicaluniversity;
 
-public class CybersecurityStudent {
+public class CybersecurityStudent extends Student {
+	public CybersecurityStudent(String Name, String Surname, String DateOfBirth) {
+		super(Name, Surname, DateOfBirth);
+		// TODO Auto-generated constructor stub
+	}
 
+	public String getNameHash() {
+		return ""; //TODO: body
+	}
 }
--- src/technicaluniversity/TelecommunicationStudent.java
+++ src/technicaluniversity/TelecommunicationStudent.java
@@ -1,5 +1,14 @@
 package technicaluniversity;
 
-public class TelecommunicationStudent {
+import java.util.HashSet;
 
+public class TelecommunicationStudent extends Student{
+	public TelecommunicationStudent(String Name, String Surname, String DateOfBirth) {
+		super(Name, Surname, DateOfBirth);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HashSet<Boolean> getNameAsMorse() {
+		return new HashSet<Boolean>(); //TODO: body
+	}
 }
