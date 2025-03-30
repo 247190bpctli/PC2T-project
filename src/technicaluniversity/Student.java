09bb70763d32d0d3bc01d010265b6d884325f56d
Vojtěch Trunda <vojtech.trunda@vojtat.cz>
2025-03-30 17:58:31 +0200

    Common student variables

--- src/technicaluniversity/Student.java
+++ src/technicaluniversity/Student.java
@@ -1,5 +1,15 @@
 package technicaluniversity;
 
-public abstract class Student {
+import java.util.HashSet;
 
+public abstract class Student {
+	private int Id;
+	private String Name;
+	private String Surname;
+	private String DateOfBirth; //TODO: make a special variable or use special one
+	private HashSet<Float> Grades;
+	
+	public Student() {
+		Grades = new HashSet<Float>();
+	}
 }
