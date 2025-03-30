31d304b510db16cdad560904e22a10c81de273c9
Vojtěch Trunda <vojtech.trunda@vojtat.cz>
2025-03-30 18:38:47 +0200

    Grades list, add grade, getters

--- src/technicaluniversity/Student.java
+++ src/technicaluniversity/Student.java
@@ -1,15 +1,45 @@
 package technicaluniversity;
 
-import java.util.HashSet;
+import java.util.List;
 
 public abstract class Student {
-	private int Id;
 	private String Name;
 	private String Surname;
 	private String DateOfBirth; //TODO: make a special variable or use special one
-	private HashSet<Float> Grades;
-	
-	public Student() {
-		Grades = new HashSet<Float>();
+	private List<Float> Grades;
+
+	public Student(String Name, String Surname, String DateOfBirth) {
+		this.Name = Name;
+		this.Surname = Surname;
+		this.DateOfBirth = DateOfBirth;
 	}
+
+	public void addGrade(int Id, float Grade) {
+		//TODO: check if grade in range 1-5, otherwise throw exception
+		Grades.add(Grade);
+	}
+
+	public String getDateOfBirth() {
+		return DateOfBirth;
+	}
+
+	//public void setDateOfBirth(String dateOfBirth) {
+	//	DateOfBirth = dateOfBirth;
+	//}
+
+	public String getSurname() {
+		return Surname;
+	}
+
+	//public void setSurname(String surname) {
+	//	Surname = surname;
+	//}
+
+	public String getName() {
+		return Name;
+	}
+
+	//public void setName(String name) {
+	//	Name = name;
+	//}
 }
